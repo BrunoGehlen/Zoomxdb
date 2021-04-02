4c9848f6ac477a11aec2f1733a37149c175b8ac9
Bruno Gehlen <tousepc@gmail.com>
2021-04-01 22:08:16 -0300

    Feat: Add mongodb connection

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const cors = require('cors')
+
+const app = express()
+const bodyParser = require('body-parser')
+
+const citiesRoute = require('./routes/cities')
+const statesRoute = require('./routes/states')
+
+require('dotenv/config')
+
+app.use(bodyParser.json())
+app.use(cors())
+
+app.use('/states', statesRoute)
+app.use('/cities', citiesRoute)
+
+app.use(express.json())
+
+mongoose.connect(process.env.DB_CONNECTION,
+    { useNewUrlParser: true, useUnifiedTopology: true}, () => {
+        console.log('connected to Database')
+})
+
+app.listen(3000)
